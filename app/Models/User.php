784110329d56a13906491aa7d54410b733fad219
Fatur <148032391+faturh@users.noreply.github.com>
2025-05-30 00:49:57 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,48 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+        // Method untuk mengecek role
+    // public function hasRole($role)
+    // {
+    //     return $this->role === $role;
+    // }
     public function skills()
     {
         return $this->belongsToMany(Skill::class);
@@ -29,5 +71,38 @@ class User extends Authenticatable
     public function preferred_type_of_work()
     {
         return $this->type_of_work;  // Menyesuaikan dengan atribut pengguna
+    }
+
+    // TAMBAHAN UNTUK COMPANY MANAGEMENT - RELASI KE COMPANY
+    public function company()
+    {
+        return $this->hasOne(Company::class);
+    }
+
+    // TAMBAHAN UNTUK JOB MANAGEMENT (jika diperlukan)
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
+
+    // TAMBAHAN SCOPE UNTUK PENCARIAN (opsional, untuk mendukung fitur search)
+    public function scopeSearch($query, $search)
+    {
+        if ($search) {
+            return $query->where(function($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                  ->orWhere('email', 'like', "%{$search}%");
+            });
+        }
+        return $query;
+    }
+
+    // TAMBAHAN SCOPE UNTUK FILTER ROLE (opsional)
+    public function scopeByRole($query, $role)
+    {
+        if ($role && $role !== 'all') {
+            return $query->where('role', $role);
+        }
+        return $query;
     }
 }
